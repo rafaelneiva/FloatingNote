c0f1f0048754ef17d2b6a43d164a7a74f748d211
rafaelneiva <rafael@zup.com.br>
2019-09-17 16:50:16 -0300

    note

--- android/app/src/main/java/com/rafaelneiva/floatingnote/floatingview/NoteActivity.kt
+++ android/app/src/main/java/com/rafaelneiva/floatingnote/floatingview/NoteActivity.kt
@@ -10,7 +10,7 @@ class NoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_note)
 
     }
 
